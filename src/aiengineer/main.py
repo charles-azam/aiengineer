38389e32e4ff3518b1155e3ef9717abaa9111bff
Charles Azam <azamcharles0@gmail.com>
2025-05-18 23:29:01 +0200

    refactor: global variable in UPPER

--- src/aiengineer/main.py
+++ src/aiengineer/main.py
@@ -1,8 +1,8 @@
 from aiengineer.core import EngineeringProject
-from aiengineer.template.nuclear_reactor import config
+from aiengineer.template.nuclear_reactor import CONFIG_REACTOR
 from aiengineer.prompts import get_prompt_ai_engineer
 
-project = EngineeringProject(config=config, system_prompt=get_prompt_ai_engineer(repo_name=config.repo_path.name))
+project = EngineeringProject(config=CONFIG_REACTOR, system_prompt=get_prompt_ai_engineer(repo_name=CONFIG_REACTOR.repo_path.name))
 
 if __name__ == "__main__":
     project.run()

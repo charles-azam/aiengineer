7814c4a764a6409d9a59d5a5c7e49135439719de
Charles Azam <azamcharles0@gmail.com>
2025-05-18 23:29:01 +0200

    refactor: global variable in UPPER

--- src/aiengineer/template/nuclear_reactor.py
+++ src/aiengineer/template/nuclear_reactor.py
@@ -1,16 +1,16 @@
 from aiengineer.config import EngineeringConfig
 from aiengineer.common import AIENGINEER_SRC_DIR
 
-nuclear_reactor_prompt = """
+PROMPT_REACTOR = """
 Let's build a small modular reactor of around 20 MW to generate electricity.
 We want a design that is as detailed as possible so that we know what to buy to our providers.
 Our design must be cheap and easy to industrialize. We require at least three high-level systems: the reactor, the primary loop and the secondary loop.
 """
 
 
-config = EngineeringConfig(
+CONFIG_REACTOR = EngineeringConfig(
     litellm_id="bedrock/us.anthropic.claude-3-7-sonnet-20250219-v1:0",
     iterations=10,
     repo_path=AIENGINEER_SRC_DIR / "reactor",
-    prompt=nuclear_reactor_prompt,
+    prompt=PROMPT_REACTOR,
 )

41a8dd05f889cab1f196698c014b3b1feaf69b41
Charles Azam <azamcharles0@gmail.com>
2025-05-30 23:04:58 +0200

    feat: add test call llm on repo

--- src/aiengineer/testing.py
+++ src/aiengineer/testing.py
@@ -1,8 +1,15 @@
+"""
+Most software engineers would rather use fixtures instead of a testing file/folder.
+
+But I can't help it, I hate fixtures. And I find it hard to explain to beginners.
+"""
+
 from pathlib import Path
 from pytest import fixture
 from aiengineer.common import AIENGINEER_SRC_DIR
 
 TESTING_PATH = AIENGINEER_SRC_DIR / "testing"
+TESTING_MODEL = "bedrock/us.anthropic.claude-3-7-sonnet-20250219-v1:0"
 
 def generate_testing_folder(testing_path: Path):
     """

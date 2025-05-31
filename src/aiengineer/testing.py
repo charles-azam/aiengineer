1bc5941acfbf7f6d8ba548483f26e395545d5b17
Charles Azam <azamcharles0@gmail.com>
2025-05-31 22:29:33 +0200

    feat: add tests for repo tools

--- src/aiengineer/testing.py
+++ src/aiengineer/testing.py
@@ -1,11 +1,10 @@
 """
-Most software engineers would rather use fixtures instead of a testing file/folder.
+Most software engineers would rather use fixtures instead of a simple testing file/folder.
 
-But I can't help it, I hate fixtures. And I find it hard to explain to beginners.
+But I can't help it, I hate fixtures. It is harder to debug And I find it hard to explain to beginners.
 """
-
+import shutil
 from pathlib import Path
-from pytest import fixture
 from aiengineer.common import AIENGINEER_SRC_DIR
 
 TESTING_PATH = AIENGINEER_SRC_DIR / "testing"
@@ -19,3 +18,36 @@ def generate_testing_folder(testing_path: Path):
         testing_path.mkdir(parents=True, exist_ok=True)
     return testing_path
     
+def initialise_empty_folder(folder_path: Path):
+    shutil.rmtree(folder_path, ignore_errors=True)
+    shutil.rmtree(folder_path.parent, ignore_errors=True)
+    folder_path.mkdir(parents=True, exist_ok=True)
+    (folder_path / "__init__.py").touch()
+    (folder_path.parent / "__init__.py").touch()
+    
+def initialise_folder_with_code() -> Path:
+    testing_dir = TESTING_PATH / "fix_repository"
+    initialise_empty_folder(testing_dir)
+    
+    file = testing_dir / "conversion.py"
+    file.write_text(
+    """
+from fix_repository.values import masse_kg
+
+def convert_kg_to_g(a: float):
+    return a * 100
+
+
+masse_g = convert_kg_to_g(masse_kg)
+
+assert masse_g*1000 == masse_kg       
+    """
+        )
+    file = testing_dir / "values.py"
+    file.write_text(
+    """
+masse_kg = 10
+print(masse_kg)
+    """
+        )
+    return testing_dir
--- tests/test_call_llm_on_repo.py
+++ tests/test_call_llm_on_repo.py
@@ -1,46 +1,13 @@
-from aiengineer.testing import TESTING_PATH, TESTING_MODEL
+from aiengineer.testing import TESTING_PATH, TESTING_MODEL, initialise_empty_folder, initialise_folder_with_code
 import shutil
 from pathlib import Path
 from aider.repo import GitRepo
 
-from aiengineer.tools.call_llm_on_repo import call_llm_on_repo, call_llm_on_repo_with_folder, fix_repository, call_llm_on_repo_with_files, RepoAsObject, RepoAsJson, get_prompt_fix_repository
-
-def initialise_folder(folder_path: Path):
-    shutil.rmtree(folder_path, ignore_errors=True)
-    shutil.rmtree(folder_path.parent, ignore_errors=True)
-    folder_path.mkdir(parents=True, exist_ok=True)
-    (folder_path / "__init__.py").touch()
-    (folder_path.parent / "__init__.py").touch()
-    
-def initialise_folder_for_testing() -> Path:
-    testing_dir = TESTING_PATH / "fix_repository"
-    initialise_folder(testing_dir)
-    
-    file = testing_dir / "conversion.py"
-    file.write_text(
-    """
-from fix_repository.values import masse_kg
-
-def convert_kg_to_g(a: float):
-    return a * 100
-
-
-masse_g = convert_kg_to_g(masse_kg)
-
-assert masse_g*1000 == masse_kg       
-    """
-        )
-    file = testing_dir / "values.py"
-    file.write_text(
-    """
-masse_kg = 10
-    """
-        )
-    return testing_dir
+from aiengineer.tools.call_llm_on_repo import call_llm_on_repo, call_llm_on_repo_with_folder, fix_repository, call_llm_on_repo_with_files, RepoAsObject, RepoAsJson, get_repo_as_json_output, get_python_errors_in_repository, get_print_outputs_in_repository
 
 def test_call_llm_on_repo():
     testing_dir = TESTING_PATH / "call_llm_on_repo"
-    initialise_folder(testing_dir)
+    initialise_empty_folder(testing_dir)
     call_llm_on_repo(
         message="""
 Create three files:
@@ -63,7 +30,7 @@ Create three files:
     
 def test_call_llm_on_repo_with_files():
     testing_dir = TESTING_PATH / "call_llm_on_repo_with_files"
-    initialise_folder(testing_dir)
+    initialise_empty_folder(testing_dir)
     call_llm_on_repo_with_files(
         message="""
 Create three files in a directory named call_llm_on_repo_with_files:
@@ -88,7 +55,7 @@ Create three files in a directory named call_llm_on_repo_with_files:
     
 def test_call_llm_on_repo_with_folder():
     testing_dir = TESTING_PATH / "call_llm_on_repo_with_folder"
-    initialise_folder(testing_dir)
+    initialise_empty_folder(testing_dir)
     call_llm_on_repo_with_folder(
         message="""
 Create three files in a directory named call_llm_on_repo_with_folder:
@@ -110,18 +77,51 @@ Create three files in a directory named call_llm_on_repo_with_folder:
     assert c == 3
     
 def test_fix_repository():
-    testing_dir = initialise_folder_for_testing()
-    from aiengineer.tools.call_llm_on_repo import fix_repository
+    testing_dir = initialise_folder_with_code()
     fix_repository(repo_path=testing_dir, litellm_id=TESTING_MODEL, edit_format="diff")
     
-def test_fix_repository_2():
-    repo = RepoAsObject.from_directory(repo_path=repo_path)
-    problems: RepoAsJson = repo.get_outputs_on_files(with_errors=True, with_outputs=False)
+def test_get_repo_as_json_output():
+    initialise_folder_with_code()
+    repo_as_json = get_repo_as_json_output(with_errors=True, with_outputs=True, repo_path=TESTING_PATH)
+    repo_as_dict = repo_as_json.to_dict()
+    assert "10" in repo_as_dict["fix_repository/values.py"].content
+    assert "No module named 'fix_repository'" in repo_as_dict["fix_repository/conversion.py"].content
+    
+def test_get_python_errors_in_repository():
+    initialise_folder_with_code()
+    message = get_python_errors_in_repository(repo_path=TESTING_PATH)
+    assert message.strip() == """
 
-    if problems:
-        print("❌ Trying and fix the problem")
-        print(problems.convert_to_flat_txt())
-        message = get_prompt_fix_repository(repo_name=repo_path.name)
-        message += problems.convert_to_flat_txt()
+**fix_repository/values.py**: 
+10
 
+
+**fix_repository/conversion.py**: 
+Error: Traceback (most recent call last):
+  File "/Users/charlesazam/charloupioupiou/aiengineer/src/aiengineer/tools/parse_repository.py", line 234, in get_outputs_on_files
+    spec.loader.exec_module(module)
+    ~~~~~~~~~~~~~~~~~~~~~~~^^^^^^^^
+  File "<frozen importlib._bootstrap_external>", line 1026, in exec_module
+  File "<frozen importlib._bootstrap>", line 488, in _call_with_frames_removed
+  File "/Users/charlesazam/charloupioupiou/aiengineer/src/testing/fix_repository/conversion.py", line 2, in <module>
+    from fix_repository.values import masse_kg
+ModuleNotFoundError: No module named \'fix_repository\'
+
+
+""".strip()
+    
+    assert "10" in message
+    assert "No module named 'fix_repository'" in message
+    
+def test_get_print_outputs_in_repository():
+    initialise_folder_with_code()
+    message = get_print_outputs_in_repository(repo_path=TESTING_PATH)
+    assert message.strip() == """
+
+**fix_repository/values.py**: 
+10
+    """.strip()
+    assert "10" in message
+    assert "No module named 'fix_repository'" not in message
+    
     

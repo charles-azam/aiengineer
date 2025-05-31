85bce57fd703f89de4672e0915898712dd29c96a
Charles Azam <azamcharles0@gmail.com>
2025-05-31 14:10:28 +0200

    feat: add loggers

--- src/aiengineer/tools/engineer_agent.py
+++ src/aiengineer/tools/engineer_agent.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 from aiengineer.tools.call_llm_on_repo import call_llm_on_repo, fix_repository, RepoAsObject
+import logging
+logger = logging.getLogger(__name__)
 
 def run_engineer_agent(repo_path: Path, question: str, system_prompt: str, litellm_id: str):
     question = system_prompt + question
@@ -7,11 +9,11 @@ def run_engineer_agent(repo_path: Path, question: str, system_prompt: str, litel
 
 def iterative_engineering_process(repo_path: Path, question: str, system_prompt: str, litellm_id: str, iterations: int = 5, trials: int = 5):
     for iteration in range(iterations):
-        print(f"--- Running iteration number {iteration} ---")
+        logger.info(f"--- Running iteration number {iteration} ---")
         i = 0
         while fix_repository(repo_path, litellm_id=litellm_id) and i < trials:
             i += 1
-            print(f"--- Attempt number {i} ---")
+            logger.warning(f"--- Attempt fix number {i} ---")
 
         repo = RepoAsObject.from_directory(repo_path=repo_path)
         outputs = repo.get_outputs_on_files(with_outputs=True, with_errors=True)

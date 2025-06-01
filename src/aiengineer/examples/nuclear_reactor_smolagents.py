fb122da7a0423de27b8119aeef0ab4de3460dc63
Charles Azam <azamcharles0@gmail.com>
2025-06-02 00:12:03 +0200

    feat: ai engineer ready

--- src/aiengineer/examples/nuclear_reactor_smolagents.py
+++ src/aiengineer/examples/nuclear_reactor_smolagents.py
@@ -1,38 +1,34 @@
-from smolagents import (CodeAgent, InferenceClientModel, LiteLLMModel,
-                        ToolCallingAgent, WebSearchTool, tool)
+from aiengineer.config import EngineeringConfig
+from aiengineer.common import AIENGINEER_SRC_DIR
+from aiengineer.smolagents_utils.main_agent import create_smolagents_engineer
 
-from aiengineer.utils.tools import (
-    ask_coder_fix_the_code_tool, ask_coder_modification_on_repo_tool,
-    get_all_print_outputs_tool, get_codebase_as_markdown_tool)
+prompt_reactor = """
+Design a modular high-temperature gas-cooled reactor (HTGR) system to decarbonizing industrial heat production. The system should:
 
-model = LiteLLMModel("bedrock/us.anthropic.claude-3-7-sonnet-20250219-v1:0")
+    Utilize TRISO fuel particles and helium coolant, operating at core temperatures up to 600°C.
 
-manager_agent = CodeAgent(
-    tools=[
-        get_codebase_as_markdown_tool,
-        ask_coder_fix_the_code_tool,
-        ask_coder_modification_on_repo_tool,
-        get_all_print_outputs_tool,
-    ],
-    model=model,
-    additional_authorized_imports=["time", "numpy", "pandas"],
-    max_steps=20,
+    Incorporate passive safety features, including heat removal and containment of fission products within fuel particles.
+
+    Be modular and scalable, suitable for installation at various industrial sites.
+
+    Provide thermal power outputs of 10, 15, or 20 MW, with a lifespan of 20 years and minimal refueling requirements.
+
+    Ensure compatibility with existing industrial heat systems, delivering heat via a secondary CO2 loop to mediums such as steam, hot air, or thermal oil.
+
+Additionally, outline the necessary infrastructure for manufacturing, assembly, and deployment, considering site requirements and regulatory compliance.
+"""
+
+
+CONFIG_REACTOR = EngineeringConfig(
+    litellm_id="bedrock/us.anthropic.claude-3-7-sonnet-20250219-v1:0",
+    iterations=20,
+    repo_path=AIENGINEER_SRC_DIR / "reactor",
+    prompt=prompt_reactor,
 )
+
+
 if __name__ == "__main__":
-    answer = manager_agent.run(
-        """
-Let's build a small modular reactor of around 20 MW to generate electricity.
-We want a design that is as detailed as possible so that we know what to buy to our providers.
-Our design must be cheap and easy to industrialize. We require at least three high-level systems: the reactor, the primary loop and the secondary loop.
+    smolagent_engineer, prompt = create_smolagents_engineer(CONFIG_REACTOR)
 
-You goal will be to use the tools to modify the codebase. You will act as a manager agent.
-
-You can get access to the codebase as a markdown string using the tool get_codebase_as_markdown_tool.
-Along with the outputs from the print statements in the codebase using the tool get_all_print_outputs_tool,
-you can ask the agent to modify the codebase using the tool ask_coder_modification_on_repo_tool.
-
-You
-
-    """
-    )
-
+    smolagent_engineer.run(prompt)
+    

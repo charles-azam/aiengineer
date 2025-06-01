e636a7fb8e7cf48f3cb14286a81d6ed457d0ef15
Charles Azam <azamcharles0@gmail.com>
2025-06-01 18:36:11 +0200

    feat: check passing functions in dict in smolagents

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -47,6 +47,48 @@ I want you to base your answer only on the answer of get_repository_map_tool. Do
     assert expected_output in tool_responses[0]["content"][0]["text"]
     clean_after_test()
 
+def test_tools_in_dict_smolagents():
+    from aiengineer.tools.call_llm_on_repo import get_repository_map
+
+    model = LiteLLMModel(TESTING_MODEL)
+
+    initialise_folder_with_working_code()
+    
+    tools_dict = dict()
+
+    @tool
+    def get_repository_map_tool() -> str:
+        """
+        Get the repository map.
+
+        Returns:
+            str: A string containing the repository map.
+        """
+        return get_repository_map(repo_path=TESTING_PATH)
+    
+    tools_dict["get_repository_map_tool"] = get_repository_map_tool
+
+    expected_output = get_repository_map_tool()
+
+    agent = CodeAgent(
+        tools=[tools_dict["get_repository_map_tool"]],
+        model=model,
+        max_steps=2,
+    )
+    agent.run(
+        f"""
+I want you to explain to me what the repository does.
+
+I want you to base your answer only on the answer of get_repository_map_tool. Do not try to use python to get more information.
+    """
+    )
+    messages = agent.write_memory_to_messages()
+    tool_responses = get_tool_responses_from_messages(messages)
+    assert len(tool_responses) == 2
+    assert expected_output in tool_responses[0]["content"][0]["text"]
+    clean_after_test()
+    
+    
 def test_get_print_outputs_in_repository():
     from aiengineer.tools.call_llm_on_repo import (
         get_print_outputs_in_repository)
